de26b4b2e8f2968427ca7dceace6ecb32ae7b60d
Geny Cabral <genycabral@gmail.com>
2017-04-15 16:36:38 -0400

    hjdodjr

--- main.js
+++ main.js
@@ -1,63 +1,26 @@
-jQuery(document).ready(function($) {
-console.log("hi")
-var data1=null
-let current = []
-    const sorter = () => {
-      console.log(data1)
-        let newData = data1
-        current.push(newData[0])
-        current.forEach((data, index) => {
-            let newnew = data.incorrect_answers
-            position = Math.floor((newnew.length +1)* Math.random())
-            console.log(position)
-            console.log(data.correct_answer)
-            newnew.splice(position, 0 ,(data.correct_answer))
-            $('.question').text(`${data.question}`)
-            if(newnew.length===2){
-            $('.row').last().addClass('remove')
-            }
-            else{
-            $('.remove').addClass('row')
-            }
-            newnew.forEach((data,index) => {
-              console.log($('.opt')[index])
-                $('.opt')[index].textContent=`${data}`
-            })
-        })
-        $('.row').each((index, question) => {
-            let selections = question.children
-            console.log(selections)
-            for (var i = 0; i < selections.length; i++) {
-              // debugger
-                if (selections[i].nodeName === "DIV") {
-                    selections[i].addEventListener('click', (e) => {
-                      console.log(newData[0].correct_answer)
-                       if( e.target.textContent === newData[0].correct_answer) {
-                         data1.shift()
-                                current.pop()
-                                sorter(data1)
-                                               console.log("win") }
-                                               else{
-                      console.log("close")}
-                    }, false)
-                }
-            }
-        })
-    }
-    $.ajax({
-            url: 'https://opentdb.com/api.php?amount=10',
-        })
-        .done(function(data) {
-            console.log(data)
-            data1 = data.results
-            sorter(data)
-            console.log("success");
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
+var electron = require('electron');
+var path = require('path');
+var url = require('url');
 
-});
+var window =  null
+
+electron.app.once('ready', function() {
+
+window = new electron.BrowserWindow({
+  show: false,
+  transparent: true,
+  frame: false
+})
+
+window.loadURL(url.format({
+  pathname: path.join(__dirname, 'index.html'),
+  protocol: 'file:',
+  slashes: true
+}))
+
+  window.once('ready-to-show', function() {
+    window.maximize()
+    window.show()
+  })
+
+})
---
+++ script.js
@@ -0,0 +1,68 @@
+$(document).ready(function($) {
+console.log("hi")
+var data1=null
+let current = []
+    const sorter = () => {
+      console.log(data1)
+        let newData = data1
+        current.push(newData[0])
+        current.forEach((data, index) => {
+            let newnew = data.incorrect_answers
+            position = Math.floor((newnew.length +1)* Math.random())
+            console.log(position)
+            console.log(data.correct_answer)
+            newnew.splice(position, 0 ,(data.correct_answer))
+
+            let ques = data.question.replace(/&quot;/g,"\"")
+            let ques2 = ques.replace(/&#039;/g,"\'")
+
+            $('.question').text(`${ques}`)
+            if(newnew.length===2){
+            $('.row').last().addClass('remove')
+            }
+            else{
+            $('.remove').addClass('row')
+            }
+            newnew.forEach((data,index) => {
+              console.log($('.opt')[index])
+              let answer = data.replace(/&quot;/g,"\"")
+                $('.opt')[index].textContent=`${answer}`
+            })
+        })
+        $('.row').each((index, question) => {
+            let selections = question.children
+            console.log(selections)
+            for (var i = 0; i < selections.length; i++) {
+              // debugger
+                if (selections[i].nodeName === "DIV") {
+                    selections[i].addEventListener('click', (e) => {
+                      console.log(newData[0].correct_answer)
+                       if( e.target.textContent === newData[0].correct_answer) {
+                         data1.shift()
+                                current.pop()
+                                sorter(data1)
+                                               console.log("win") }
+                                               else{
+                      console.log("close")}
+                    }, false)
+                }
+            }
+        })
+    }
+    $.ajax({
+            url: 'https://opentdb.com/api.php?amount=10',
+        })
+        .done(function(data) {
+            console.log(data)
+            data1 = data.results
+            sorter(data)
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+});
