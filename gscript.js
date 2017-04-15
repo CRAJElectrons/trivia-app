f640d25fe4845eebae46fd0abeb1322b32428477
Jamal Chapman <jchapman0819@gmail.com>
2017-04-15 15:31:19 -0400

    Trvia Game questions and answers complete with on click for right and wrong answers

--- gscript.js
+++ gscript.js
@@ -38,52 +38,5 @@ setInterval(() => {
 $('.tri' ).click(() => {
   $('.catm').toggleClass('hide');
 });
-
-    console.log("hi")
-    const sorter = (data) => {
-        let newData = data.results
-        newData.forEach((data, index) => {
-            let newnew = data.incorrect_answers.map((data) => {
-                return $(`<div>${data}</div>`)
-            })
-            position = Math.floor((
-              newnew.length +1)* Math.random())
-            console.log(position)
-            newnew.splice(position, 0 ,$(`<div>${data.correct_answer}</div>`))
-            console.log((newnew))
-            $('.container').append($(`<div class=questions><p>${data.question}</p></div>`))
-            newnew.sort((a,b)=>{ a.length < b.length}).forEach((data) => {
-                $('.questions').last().append(data)
-            })
-        })
-        $('.questions').each((index, question) => {
-            let selections = question.children
-            for (var i = 0; i < selections.length; i++) {
-                if (selections[i].nodeName === "DIV") {
-                    selections[i].addEventListener('click', (e) => {
-                      console.log(newData[index].correct_answer)
-                        e.target.textContent === newData[index].correct_answer ? console.log("win") : console.log("close")
-                    }, false)
-                }
-            }
-        })
-    }
-    $.ajax({
-            url: 'https://opentdb.com/api.php?amount=10',
-        })
-        .done(function(data) {
-            console.log(data)
-            sorter(data)
-            console.log("success");
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
-
-
-
 console.log('JQ Linked');
 });
