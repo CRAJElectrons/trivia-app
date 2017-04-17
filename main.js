5d29df4779402f28afe250ab8c9bedf0ab83b650
Jamal Chapman <jchapman0819@gmail.com>
2017-04-17 10:04:40 -0400

    window dont close electron

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var url = require('url');
 
 var window =  null
 
-electron.app.once('ready', function() {
+electron.app.once('ready', createWindow=()=> {
 
 window = new electron.BrowserWindow({
   show: false,
@@ -23,5 +23,21 @@ window.loadURL(url.format({
     window.maximize()
     window.show()
   })
-
 })
+  electron.app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+electron.app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (window !== null) {
+    createWindow()
+  }
+})
+
+
